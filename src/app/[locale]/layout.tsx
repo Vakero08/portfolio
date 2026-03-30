0ae75f043209d2ad27ccc4783c893aaa86fb26e3
Jhonatan Duran <jhona_usa16@hotmail.com>
2026-03-30 16:47:35 -0500

    feat: :art: Integrate Microsoft Clarity analytics script for production environment

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -8,6 +8,8 @@ import { routing } from "@/i18n/routing";
 import { notFound } from "next/navigation";
 import { setRequestLocale, getTranslations } from "next-intl/server";
 import { Analytics } from "@vercel/analytics/next";
+import Script from "next/script";
+
 
 export async function generateMetadata({ params }: { params: Promise<{ locale: string }> }) {
   const { locale } = await params;
@@ -50,6 +52,18 @@ export default async function RootLayout({
             <Header />
 
             {children}
+            {/* Microsoft Clarity - Se inyecta SOLO en entorno de Producción */}
+            {process.env.NODE_ENV === 'production' && process.env.NEXT_PUBLIC_CLARITY_ID && (
+              <Script id="microsoft-clarity" strategy="afterInteractive">
+                {`
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "${process.env.NEXT_PUBLIC_CLARITY_ID}");
+            `}
+              </Script>
+            )}
             <Analytics />
           </NextIntlClientProvider>
           <Footer />
